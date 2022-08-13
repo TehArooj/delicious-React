14ca34ddba38f1a1e9a3b8da198e59dc864ac1fa
Tehreem Arooj <mazharm0786@gmail.com>
2022-08-13 11:20:23 +0500

    feat:delicioussss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from "react";
 
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import { BrowserRouter, Link } from "react-router-dom";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>;
+    <div className="App">
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>delicioussss</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 };
 
+const Logo = styled.div`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 export default App;
